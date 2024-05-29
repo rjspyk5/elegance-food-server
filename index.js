e38184d25b5c63a3e7cb6db8a7beecc4903ca2de
rjspyk5 <rjspyk5@gmail.com>
2024-05-29 19:00:45 +0600

    create api to get single menu iteam and face challenges here

--- index.js
+++ index.js
@@ -91,6 +91,17 @@ async function run() {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
+    app.get("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.findOne(query);
+      if (!result) {
+        const queryWithOutObjectId = { _id: id };
+        const result = await menuCollection.findOne(queryWithOutObjectId);
+        return result;
+      }
+      res.send(result);
+    });
 
     app.post("/menu", async (req, res) => {
       const data = req.body;
